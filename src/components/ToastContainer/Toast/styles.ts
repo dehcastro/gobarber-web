eab88e309fbb1a714e809b9360a3ebc44c6d9dba
André de Castro <andre_vc@hotmail.com>
2020-10-08 18:47:19 -0300

    :heavy_check_mark: Nível 05 | Recuperação de senha | All classes check! ;)

---
+++ src/components/ToastContainer/Toast/styles.ts
@@ -0,0 +1,85 @@
+import styled, { css } from 'styled-components';
+import { animated } from 'react-spring';
+
+interface ContainerProps {
+  type?: 'success' | 'warning' | 'danger' | 'info';
+  $hasDescription: boolean;
+}
+
+const toastTypeVariations = {
+  info: css`
+    background: #ebf8ff;
+    color: #3172b7;
+  `,
+  success: css`
+    background: #e6fffa;
+    color: #2e656a;
+  `,
+  warning: css`
+    background: #fffce3;
+    color: #b79821;
+  `,
+  danger: css`
+    background: #fddede;
+    color: #c53030;
+  `,
+};
+
+export const Container = styled(animated.div)<ContainerProps>`
+  width: 360px;
+
+  position: relative;
+  padding: 16px 30px 16px 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${props => toastTypeVariations[props.type || 'info']}
+
+  > svg {
+    margin: 4px 12px 0 0;
+  }
+
+  div {
+    flex: 1;
+
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      opacity: 0.8;
+      line-height: 20px;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 12px;
+    opacity: 0.6;
+    border: 0;
+    background: transparent;
+    color: inherit;
+  }
+
+  ${props =>
+    !props.$hasDescription &&
+    css`
+      display: flex;
+      align-items: center;
+      padding-right: 50px;
+
+      svg {
+        margin-top: 0;
+      }
+
+      button {
+        display: flex;
+        top: auto;
+      }
+    `}
+`;
---
+++ src/pages/ResetPassword/index.tsx
@@ -0,0 +1,114 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import api from '../../services/api';
+
+import { Container, Content, Background, AnimationContainer } from './styles';
+
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const history = useHistory();
+  const location = useLocation();
+
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          password: Yup.string().min(6, 'Senha obrigatória'),
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password')],
+            'Senhas precisam ser iguais',
+          ),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/passwords/reset', {
+          password,
+          password_confirmation,
+          token,
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'danger',
+          title: 'Erro ao resetar senha.',
+          description: 'Ocorreu um erro ao resetar sua senha, tente novamente.',
+        });
+      }
+    },
+    [addToast, history, location],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form onSubmit={handleSubmit} ref={formRef}>
+            <h1>Resetar senha</h1>
+
+            <Input
+              type="password"
+              icon={FiLock}
+              name="password"
+              placeholder="Nova Senha"
+            />
+
+            <Input
+              type="password"
+              icon={FiLock}
+              name="password_confirmation"
+              placeholder="Confirmação da Senha"
+            />
+
+            <Button type="submit">Alterar Senha</Button>
+          </Form>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;
